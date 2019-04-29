4fc3979a72ecc3502e2a3d7eb552fe4f3f310296
at-moon <894298646@qq.com>
2019-04-29 16:03:56 +0800

    引入activiti-rest

--- src/main/java/com/eshore/activiti6/Activiti6Application.java
+++ src/main/java/com/eshore/activiti6/Activiti6Application.java
@@ -2,8 +2,14 @@ package com.eshore.activiti6;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication(exclude = {org.activiti.spring.boot.SecurityAutoConfiguration.class})
+@SpringBootApplication(exclude = {
+		org.activiti.spring.boot.SecurityAutoConfiguration.class,
+		LiquibaseAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class
+})
 public class Activiti6Application {
 
 	public static void main(String[] args) {
--- src/main/java/com/eshore/activiti6/config/DataSourceConfig.java
+++ src/main/java/com/eshore/activiti6/config/DataSourceConfig.java
@@ -1,16 +1,27 @@
 package com.eshore.activiti6.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import org.hibernate.ejb.HibernatePersistence;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaDialect;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
+import java.util.Properties;
 
 @ComponentScan
 @Configuration
+@EnableJpaRepositories({ "org.activiti.app.repository" })
+@EnableTransactionManagement
 public class DataSourceConfig {
 
     @Autowired
@@ -24,5 +35,33 @@ public class DataSourceConfig {
         dataSource.setPassword(environment.getProperty("spring.datasource.password"));
         dataSource.setDriverClassName(environment.getProperty("spring.datasource.driver-class-name"));
         return dataSource;
+    }
+
+
+    @Bean(name = "entityManagerFactory")
+    public EntityManagerFactory entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean lcemfb = new LocalContainerEntityManagerFactoryBean();
+        lcemfb.setPersistenceProvider(new HibernatePersistence());
+        lcemfb.setPersistenceUnitName("persistenceUnit");
+        lcemfb.setDataSource(getDataSource());
+        lcemfb.setJpaDialect(new HibernateJpaDialect());
+        lcemfb.setJpaVendorAdapter(jpaVendorAdapter());
+
+        Properties jpaProperties = new Properties();
+        jpaProperties.put("hibernate.cache.use_second_level_cache", false);
+        jpaProperties.put("hibernate.generate_statistics", environment.getProperty("hibernate.generate_statistics", Boolean.class, false));
+        lcemfb.setJpaProperties(jpaProperties);
+
+        lcemfb.setPackagesToScan("org.activiti.app.domain");
+        lcemfb.afterPropertiesSet();
+        return lcemfb.getObject();
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
+        jpaVendorAdapter.setShowSql(environment.getProperty("hibernate.show_sql", Boolean.class, false));
+        jpaVendorAdapter.setDatabasePlatform(environment.getProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect"));
+        return jpaVendorAdapter;
     }
 }
